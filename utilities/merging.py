8a2b2d31218b85e21f505056706537105528a55d
albertolorenzini <a.lorenzini92@gmail.com>
2021-05-25 17:59:28 +0200

    modified merging.py file

--- utilities/merging.py
+++ utilities/merging.py
@@ -1,3 +1,21 @@
+'''
+merging.py merge different annotations and images dataset in a unique one 
+
+it request COCO-assistant to work, you can install it using pip install coco-assistant or from source: https://github.com/ashnair1/COCO-Assistant
+
+author = Alberto Lorenzini
+'''
+
+'''
+usage: create a directory and move this file inside the just created directory, then insiede this directory create two folder called 'images' and 'annotations', 
+put all your images in the 'images' folder and all your annotations files in the 'annotations' folder
+Then run the following command:
+
+    python merging.py
+    
+'''
+
+
 import os
 from coco_assistant import COCO_Assistant
 
